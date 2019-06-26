1f14ca8eb9daa8ba4869b5373c5b52ba6659c5dc
mkbeh <mkbehforever@gmail.com>
2019-06-26 16:47:26 +0300

    Updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,20 +14,9 @@
 
 import os
 import sys
-from unittest.mock import MagicMock
 
 
 sys.path.insert(0, os.path.abspath('../'))
-
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-
-# MOCK_MODULES = ['aiohttp', 'ujson']
-# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- Project information -----------------------------------------------------
