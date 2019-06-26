2673902a298f99ba226b80cda4aa8c8345137bd1
mkbeh <mkbehforever@gmail.com>
2019-06-26 16:45:05 +0300

    Updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,8 @@ class Mock(MagicMock):
         return MagicMock()
 
 
-MOCK_MODULES = ['aiohttp', 'ujson']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+# MOCK_MODULES = ['aiohttp', 'ujson']
+# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- Project information -----------------------------------------------------
